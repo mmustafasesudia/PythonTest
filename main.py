43cc268c143426370a084dff19afd7de2d59b575
Muhammad Mustafa <mmustafasesudia@gmail.com>
2024-01-04 15:04:35 +0500

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from langchain.vectorstores import FAISS
 from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
 import io
+import os
 
 os.environ.get('OPEN_AI_KEY')
 
